3746858288d505782489800c14a0fa57fd0bb70f
Abhay Zalera <160293769+AbhayZalera@users.noreply.github.com>
2025-03-24 11:53:06 +0530

    Second commit

--- app/Services/Notify.php
+++ app/Services/Notify.php
@@ -4,7 +4,7 @@ namespace  App\Services;
 
 class Notify
 {
-    //Created Notifivation
+    //Created Notification
     static function createdNotification()
     {
         return notyf()->addSuccess('Created Successfully ⚡️', 'Success!');
